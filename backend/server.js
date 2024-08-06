13407068cb186d58e66c1c19f72e4aae685fd3d6
Nhuz11 <nhuzbajra59@gmail.com>
2024-08-06 22:01:31 +0545

    routes in api and postman check

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const workoutRoutes= require('../backend/routes/work')
 
 //Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 //express app
 const app=express()
 
 //middleware
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get('/',(req,res)=>{
-    res.json({msg:"Welcome to app mf" })
-})
+// app.get('/',(req,res)=>{
+//     res.json({msg:"Welcome to app mf" })
+// })
+
+app.use('/api/workouts',workoutRoutes)
 
 //listen for request
 app.listen(process.env.PORT,()=>{
